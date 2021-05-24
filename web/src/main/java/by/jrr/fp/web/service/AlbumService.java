6602a6c2e5ce7eddf8065ddbb76270fb331c542c
max-java <6666350@gmail.com>
2021-05-24 15:27:52 +0300

    adds final project template

---
+++ web/src/main/java/by/jrr/fp/web/service/AlbumService.java
@@ -0,0 +1,23 @@
+package by.jrr.fp.web.service;
+
+import by.jrr.fp.web.model.Photo;
+import by.jrr.fp.web.proxy.ProcessorProxy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class AlbumService {
+
+    @Autowired
+    ProcessorProxy processorProxy;
+
+    public List<Photo> findAll() {
+        PagedModel<Photo> result = processorProxy.getAllPhotos();
+        result.getContent();
+        return new ArrayList<>(result.getContent());
+    }
+}
